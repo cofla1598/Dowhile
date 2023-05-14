dbebb683d9136e6915a69add5d6e05c186c7af24
java-positivo-negativo <aguileraandres86@gmail.com>
2023-05-13 21:17:37 -0500

    aumenta

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+    aunmentarYMostrar();
+
+    }
+
+public static void aunmentarYMostrar(){
+        int numeroDoWhile=0;
+        do {
+            numeroDoWhile++;
+            System.out.println(numeroDoWhile);
+        }while (numeroDoWhile<3);
+
+}
+}
